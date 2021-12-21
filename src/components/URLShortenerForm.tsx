c040270c0b75a9afd74adbca29348139c7356727
Zay Min Maw <zayminmaw77@gmail.com>
2021-12-21 23:17:58 +0630

    init

---
+++ src/components/URLShortenerForm.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+interface URLShotenerFormProps {}
+
+const URLShotenerForm: FunctionComponent<URLShotenerFormProps> = () => {
+  return <h1>Hello</h1>;
+};
+
+export default URLShotenerForm;
